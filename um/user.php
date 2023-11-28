3a784dfd51a12101eedad62cbbe2cc3aa6544603
Snat <120643725+SnatMTE@users.noreply.github.com>
2023-11-28 22:30:17 +0000

    Update user.php

--- um/user.php
+++ um/user.php
@@ -20,4 +20,34 @@ function snat_framework_hash_password($password, $cost = 10)
     return $hash;
 }
 
+/**
+ * Send an email using the mail() function
+ * 
+ * @param string $to      The recipient email address
+ * @param string $subject The email subject
+ * @param string $message The email message
+ * @param string $headers Additional headers (optional)
+ * 
+ * @return bool Returns true if the email was sent successfully, false otherwise
+ */
+function snat_framework_send_email($to, $subject, $message, $headers = '')
+{
+    // Additional headers
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+
+    // Additional headers (optional)
+    // $headers .= "From: " . $from . "\r\n";
+    // $headers .= "Cc: " . $cc . "\r\n";
+    // $headers .= "Bcc: " . $bcc . "\r\n";
+
+    // Send email
+    return mail($to, $subject, $message, $headers);
+}
+
+if (snat_framework_send_email($to, $subject, $message, $headers)) {
+    echo 'Email sent successfully.';
+} else {
+    echo 'Email could not be sent.';
+}
 ?>
